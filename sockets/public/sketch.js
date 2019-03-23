63ca65ac0e9857a453d0ee058b3e44908124e2f4
bunnieabc <bunnieabc@gmail.com>
2019-03-23 11:43:07 -0400

    fix camera problem, have client and server

--- sockets/public/sketch.js
+++ sockets/public/sketch.js
@@ -3,14 +3,70 @@
 // Daniel Shiffman
 
 // Keep track of our socket connection
-var socket;
+var socket, socket2;
+var video;
+var slider;
+
+
 
 function setup() {
-  createCanvas(400, 400);
-  background(0);
+  canvas = createCanvas(640, 480, WEBGL);
+  canvas.id('p5canvas');
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.id('p5video');
+  //video.hide();
+  //slider = createSlider(0, 1, 0.5, 0.01);
+  //slider.id('blur-slider');
+
+  var seriously = new Seriously();
+
+  var src = seriously.source('#p5video');
+  var target = seriously.target('#p5canvas');
+
+  // var blur = seriously.effect('blur');
+  // blur.amount = '#blur-slider';
+  // blur.source = src;
+  // target.source = blur;
+
+  var chroma = seriously.effect('chroma');
+  chroma.source = src;
+  target.source = chroma;
+  var r = 98 / 255;
+  var g = 175 / 255;
+  var b = 135 / 255;
+  chroma.screen = [r,g,b,1];
+
+
+  seriously.go();
+
+
+
+
+
+
+
+
+
+  socket2 = io.connect('http://localhost:3000');
+  socket2.on('ct',
+    // When we receive data
+    function(ctt) {
+      console.log("Client num: " + ctt);
+      // Draw a blue circle
+      
+      //socket2.emit('ct',ctt);
+
+    }
+  );
+  
+
+
+
+  
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
+  /*socket = io.connect('http://localhost:3000');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
@@ -22,8 +78,10 @@ function setup() {
       noStroke();
       ellipse(data.x, data.y, 20, 20);
     }
-  );
+  );*/
 }
+
+
 
 function draw() {
   // Nothing
@@ -31,17 +89,17 @@ function draw() {
 
 function mouseDragged() {
   // Draw some white circles
-  fill(255);
+  /*fill(255);
   noStroke();
   ellipse(mouseX,mouseY,20,20);
   // Send the mouse coordinates
-  sendmouse(mouseX,mouseY);
+  sendmouse(mouseX,mouseY);*/
 }
 
 // Function for sending to the socket
 function sendmouse(xpos, ypos) {
   // We are sending!
-  console.log("sendmouse: " + xpos + " " + ypos);
+  /*console.log("sendmouse: " + xpos + " " + ypos);
   
   // Make a little object with  and y
   var data = {
@@ -50,5 +108,5 @@ function sendmouse(xpos, ypos) {
   };
 
   // Send that object to the socket
-  socket.emit('mouse',data);
+  socket.emit('mouse',data);*/
 }
--- sockets/server.js
+++ sockets/server.js
@@ -10,6 +10,10 @@ var app = express();
 // process.env.PORT is related to deploying on heroku
 var server = app.listen(process.env.PORT || 3000, listen);
 
+////test number of socket
+var socketNum = 0;
+///
+
 // This call back just tells us that the server has started
 function listen() {
   var host = server.address().address;
@@ -28,27 +32,58 @@ var io = require('socket.io')(server);
 // This is run for each individual user that connects
 io.sockets.on('connection',
   // We are given a websocket object in our function
-  function (socket) {
   
-    console.log("We have a new client: " + socket.id);
-  
-    // When this user emits, client side: socket.emit('otherevent',some data);
-    socket.on('mouse',
-      function(data) {
-        // Data comes in as whatever was sent, including objects
-        console.log("Received: 'mouse' " + data.x + " " + data.y);
-      
-        // Send it to all other clients
-        socket.broadcast.emit('mouse', data);
-        
-        // This is a way to send to everyone including sender
-        // io.sockets.emit('message', "this goes to everyone");
 
-      }
-    );
-    
-    socket.on('disconnect', function() {
-      console.log("Client has disconnected");
-    });
-  }
+    function (socket) {
+      socketNum = socketNum + 1;
+    // When this user emits, client side: socket.emit('otherevent',some data);
+      io.to(socket.id).emit('ct', socketNum);
+
+      socket.on('mouse',
+        function(data) {
+          // Data comes in as whatever was sent, including objects
+          console.log("Received: 'mouse' " + data.x + " " + data.y);
+        
+          // Send it to all other clients
+          socket.broadcast.emit('mouse', data);
+          
+          // This is a way to send to everyone including sender
+          // io.sockets.emit('message', "this goes to everyone");
+
+        }
+      );
+      
+      socket.on('disconnect', function() {
+        socketNum = socketNum - 1;
+        console.log("Client has disconnected");
+      });
+    }
+
+    /*,
+
+    function (socket2) {
+      socketNum = socketNum + 1;
+      console.log("We have a new client: " + socket.id);
+      console.log("SocketNum: " + socketNum);
+
+
+      socket2.on('ct',
+        function(ctt) {
+          // Data comes in as whatever was sent, including objects
+          console.log("ctt: " + socketNum);
+          // Send it to all other clients
+          socket2.broadcast.emit('ct', socketNum);
+          
+          // This is a way to send to everyone including sender
+          // io.sockets.emit('message', "this goes to everyone");
+        });
+
+      // When this user emits, client side: socket.emit('otherevent',some data);
+      
+      socket2.on('disconnect', function() {
+        console.log("Client has disconnected");
+    });}
+    */
+
+
 );
